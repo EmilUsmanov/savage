4ad6d049ff9292287259d895613137ee8429d987
Emil Usmanov <emilusmanova@gmail.com>
2024-01-24 13:12:40 +0500

    pomenyal na obosral

--- puk.c
+++ puk.c
@@ -8,7 +8,7 @@ int main(void) {
   switch (s) {
           
     case '1' : printf("плохо\n"); break;
-    case '2' : printf("неудовлетворительно\n"); break;
+    case '2' : printf("obosrano\n"); break;
     case '3' : printf("удовлетворительно\n"); break;
     case '4' : printf("хорошо\n"); break;
     case '5' : printf("отлично\n"); break;      
